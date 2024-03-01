1896e9123fe0bdb5b0e3a1a728696f4d518c62a5
BrianCV48 <brianccapaviza@gmail.com>
2024-03-01 09:40:43 -0500

    app blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,100 +7,95 @@
 @endif
 
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  @if (env('IS_DEMO'))
+  <x-demo-metas></x-demo-metas>
+  @endif
 
-    @if (env('IS_DEMO'))
-    <x-demo-metas></x-demo-metas>
-    @endif
-
-    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-    <link rel="icon" type="image/png" href="../assets/img/favicon.png">
-    <title>
-        College
-    </title>
-    <!--     Fonts and icons     -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
-    <!-- Nucleo Icons -->
-    <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
-    <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
-    <!-- Font Awesome Icons -->
-    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-    <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
-    <!-- CSS Files -->
-    <link id="Paginastyle" href="../assets/css/soft-ui-dashboard.css?v=1.0.3" rel="stylesheet" />
-    <!-- CSS estilos -->
-    <link href="../assets/css/estilos.css" rel="stylesheet" />
-
-    <!--     DataTable CSS   -->
-    <!-- <link rel="stylesheet" href="../assets/plugin/datatable/datatables.min.css"> -->
-    <link href="../assets/plugin/datatable/buttons/css/buttons.dataTables.min.css" rel="stylesheet">
-    <link href="../assets/plugin/datatable/buttons/css/buttons.bootstrap5.min.css" rel="stylesheet">
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <!--     DataTable CSS   -->
+  <link rel="stylesheet" href="../assets/plugin/datatable/datatables.min.css">
+  <link href="../assets/plugin/datatable/buttons/css/buttons.dataTables.min.css" rel="stylesheet">
+  <link href="../assets/plugin/datatable/buttons/css/buttons.bootstrap5.min.css" rel="stylesheet">
+  <title>
+    College
+  </title>
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+  <!-- Nucleo Icons -->
+  <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link id="Paginastyle" href="../assets/css/soft-ui-dashboard.css?v=1.0.3" rel="stylesheet" />
+  <!-- CSS estilos -->
+  <link href="../assets/css/estilos.css" rel="stylesheet" />
+  <link href="../assets/css/sweetalert.css" rel="stylesheet" />
+  
 </head>
 
-<body style="overflow-y: hidden" class="g-sidenav-show  bg-gray-100 {{ \Request::is('rtl') ? 'rtl' : (Request::is('virtual-reality') ? 'virtual-reality' : '') }} ">
-    @auth
-    @yield('auth')
-    @endauth
-    @guest
-    @yield('guest')
-    @endguest
+<body style="overflow-y: hidden" class="g-sidenav-show  bg-gray-100 {{ (\Request::is('rtl') ? 'rtl' : (Request::is('virtual-reality') ? 'virtual-reality' : '')) }} ">
+  @auth
+  @yield('auth')
+  @endauth
+  @guest
+  @yield('guest')
+  @endguest
 
-    {{-- @if (session()->has('success'))
+  {{-- @if(session()->has('success'))
     <div x-data="{ show: true}"
         x-init="setTimeout(() => show = false, 4000)"
         x-show="show"
         class="position-fixed bg-success rounded right-3 text-sm py-2 px-4">
       <p class="m-0">{{ session('success')}}</p>
-    </div>
-    @endif --}}
-    <!--   Jquery 3.17   -->
-    <script src="../assets/js/plugins/jquery.min.js"></script>
-    <!--   Core JS Files   -->
-    <script src="../assets/js/core/popper.min.js"></script>
-    <script src="../assets/js/core/bootstrap.min.js"></script>
-    <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
-    <script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
-    <script src="../assets/js/plugins/fullcalendar.min.js"></script>
-    <script src="../assets/js/plugins/chartjs.min.js"></script>
+  </div>
+  @endif --}}
+  <!--   Jquery 3.17   -->
+  <script src="../assets/js/plugins/jquery.min.js"></script>
+  <!--   Core JS Files   -->
+  <script src="../assets/js/core/popper.min.js"></script>
+  <script src="../assets/js/core/bootstrap.min.js"></script>
+  <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
+  <script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
+  <script src="../assets/js/plugins/fullcalendar.min.js"></script>
+  <script src="../assets/js/plugins/chartjs.min.js"></script>
+
+  @stack('rtl')
+  @stack('dashboard')
+  <script>
+    var win = navigator.platform.indexOf('Win') > -1;
+    if (win && document.querySelector('#sidenav-scrollbar')) {
+      var options = {
+        damping: '0.5'
+      }
+      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+    }
+  </script>
+
+  <!-- Github buttons -->
+  <script async defer src="https://buttons.github.io/buttons.js"></script>
+  <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example Paginas etc -->
+  <script src="../assets/js/soft-ui-dashboard.min.js?v=1.0.3"></script>
+  <script src="../assets/js/college.js"></script>
 
 
+  <script src="../assets/js/sistema/dashboard.js"></script>
+  <script src="../assets/js/sistema/placa.js"></script>
 
-    @stack('rtl')
-    @stack('dashboard')
-    <script>
-        var win = navigator.platform.indexOf('Win') > -1;
-        if (win && document.querySelector('#sidenav-scrollbar')) {
-            var options = {
-                damping: '0.5'
-            }
-            Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
-        }
-    </script>
+  <!--     DataTable JS     -->
+  <script src="../assets/plugin/datatable/datatables.min.js"></script>
+  <script src="../assets/plugin/datatable/buttons/js/dataTables.buttons.min.js"></script>
+  <script src="../assets/plugin/datatable/buttons/js/buttons.html5.min.js"></script>
+  <script src="../assets/plugin/datatable/buttons/js/buttons.print.min.js"></script>
+  <script src="../assets/plugin/datatable/pdfmake/pdfmake.min.js"></script>
+  <script src="../assets/plugin/datatable/pdfmake/vfs_fonts.js"></script>
 
-    <!-- Github buttons -->
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
-    <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example Paginas etc -->
-    <script src="../assets/js/soft-ui-dashboard.min.js?v=1.0.3"></script>
-    <script src="../assets/js/college.js"></script>
-
-
-    <script src="../assets/js/sistema/dashboard.js"></script>
-    <script src="../assets/js/sistema/placa.js"></script>
-    <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> -->
-
-    <!--     DataTable JS     -->
-    <script src="../assets/plugin/datatable/datatables.min.js"></script>
-    <script src="../assets/plugin/datatable/buttons/js/dataTables.buttons.min.js"></script>
-    <script src="../assets/plugin/datatable/buttons/js/buttons.html5.min.js"></script>
-    <script src="../assets/plugin/datatable/buttons/js/buttons.print.min.js"></script>
-    <script src="../assets/plugin/datatable/pdfmake/pdfmake.min.js"></script>
-    <script src="../assets/plugin/datatable/pdfmake/vfs_fonts.js"></script>
-
-
-    <!--     SweetAlert JS     -->
-    <script src="../assets/js/plugins/sweetalert.min.js"></script>
-
-</body>
-
+  
+  <!--     SweetAlert JS     -->
+  <script src="../assets/js/plugins/sweetalert.min.js"></script>
 </html>
